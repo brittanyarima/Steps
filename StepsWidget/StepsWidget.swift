d3f5ad4ac256582f734130c53e4c44b8247aa050
brittanyarima <brittanyarima@gmail.com>
2022-12-29 11:07:01 -0800

    WidgetUI

--- StepsWidget/StepsWidget.swift
+++ StepsWidget/StepsWidget.swift
@@ -10,22 +10,25 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), steps: 7200)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), steps: 7200)
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
+        var steps = StepsViewModel().currentSteps
+        var goal = StepsViewModel().goal
+
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+            let entry = SimpleEntry(date: entryDate, steps: steps)
             entries.append(entry)
         }
 
@@ -36,13 +39,35 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let steps: Int
 }
 
 struct StepsWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack {
+            Circle()
+                .stroke(.indigo.opacity(0.1), lineWidth: 10)
+
+            Circle()
+                .trim(from: 0, to: CGFloat(7200) / CGFloat(10000))
+                .stroke(.indigo, style: StrokeStyle(lineWidth: 10, lineCap: .round))
+                .rotationEffect(Angle(degrees: -90.0))
+
+            VStack {
+                Text("7,200")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(.indigo)
+
+
+                Text("Steps")
+                    .foregroundColor(.secondary)
+                    .font(.caption)
+            }
+        }
+        .padding()
     }
 }
 
@@ -60,7 +85,7 @@ struct StepsWidget: Widget {
 
 struct StepsWidget_Previews: PreviewProvider {
     static var previews: some View {
-        StepsWidgetEntryView(entry: SimpleEntry(date: Date()))
+        StepsWidgetEntryView(entry: SimpleEntry(date: Date(), steps: 7200))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
