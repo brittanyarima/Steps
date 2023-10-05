b24e756eb512c056773e44a9b747fca3b103dd34
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2023-10-05 17:06:19 -0600

    Added Test Target

---
+++ StepsTests/StepsTests.swift
@@ -0,0 +1,20 @@
+//
+//  StepsTests.swift
+//  StepsTests
+//
+//  Created by Daniel Lyons on 10/5/23.
+//
+
+
+import XCTest
+import Dependencies
+import DependenciesAdditions
+@testable import Steps
+
+final class StepsTests: XCTestCase {
+    func testNotificationsToggle() async {
+        XCTAssertEqual(true, true)
+    }
+    
+
+}
