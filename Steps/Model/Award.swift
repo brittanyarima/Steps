7c576a884d7b2b0272785435a0c0d6328cc9bcfc
Brittany Rima <76922883+brittanyarima@users.noreply.github.com>
2023-10-11 23:16:32 -0700

    Update Award.swift

--- Steps/Model/Award.swift
+++ Steps/Model/Award.swift
@@ -7,23 +7,91 @@
 
 import SwiftUI
 
-struct Award {
-    let name: String
-    let description: String
-    let image: String
+enum Award: String, CaseIterable {
+    case firstStep
+    case goal
+    case doubleTrouble
+    case threes
+    case perfectweek
+    case messi
+    case motivated
+    case firstGoal
+    case dreamerGoal
+    case goGetter
+
+    var name: String {
+        switch self {
+        case .firstStep:
+            return Constants.firstStepsName
+        case .goal:
+            return Constants.goalName
+        case .doubleTrouble:
+            return Constants.doubleTroubleName
+        case .threes:
+            return Constants.threesName
+        case .perfectweek:
+            return Constants.perfectWeekName
+        case .messi:
+            return Constants.messiName
+        case .motivated:
+            return Constants.motivatedName
+        case .firstGoal:
+            return Constants.firstGoalName
+        case .dreamerGoal:
+            return Constants.dreamerGoalName
+        case .goGetter:
+            return Constants.goGetterName
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .firstStep:
+            return Constants.firstStepsDescription
+        case .goal:
+            return Constants.goalDescription
+        case .doubleTrouble:
+            return Constants.doubleTroubleDescription
+        case .threes:
+            return Constants.threesDescription
+        case .perfectweek:
+            return Constants.perfectWeekDescription
+        case .messi:
+            return Constants.soccerFieldDescription
+        case .motivated:
+            return Constants.motivatedDescription
+        case .firstGoal:
+            return Constants.firstGoalDescription
+        case .dreamerGoal:
+            return Constants.dreamerGoalDescription
+        case .goGetter:
+            return Constants.goGetterDescription
+        }
+    }
+
+    var image: String {
+        switch self {
+        case .firstStep:
+            return "figure.walk"
+        case .goal:
+            return "soccerball"
+        case .doubleTrouble:
+            return "figure.walk.motion"
+        case .threes:
+            return "figure.dance"
+        case .perfectweek:
+            return "medal"
+        case .messi:
+            return "sportscourt"
+        case .motivated:
+            return "checklist.unchecked"
+        case .firstGoal:
+            return "checkmark.seal"
+        case .dreamerGoal:
+            return "list.bullet.rectangle"
+        case .goGetter:
+            return "text.badge.checkmark.rtl"
+        }
+    }
 }
 
-struct AwardData {
-    static let awards = [
-        Award(name: Constants.firstStepsName, description: Constants.firstStepsDescription, image: "figure.walk"),
-        Award(name: Constants.goalName, description: Constants.goalDescription, image: "soccerball"),
-        Award(name: Constants.doubleTroubleName, description: Constants.doubleTroubleDescription, image: "figure.walk.motion"),
-        Award(name: Constants.threesName, description: Constants.threesDescription, image: "figure.dance"),
-        Award(name: Constants.perfectWeekName, description: Constants.perfectWeekDescription, image: "medal"),
-        Award(name: Constants.messiName, description: Constants.soccerFieldDescription, image: "sportscourt"),
-        Award(name: Constants.motivatedName, description: Constants.motivatedDescription, image: "checklist.unchecked"),
-        Award(name: Constants.firstGoalName, description: Constants.firstGoalDescription, image: "checkmark.seal"),
-        Award(name: Constants.dreamerGoalName, description: Constants.dreamerGoalDescription, image: "list.bullet.rectangle"),
-        Award(name: Constants.goGetterName, description: Constants.goGetterDescription, image: "text.badge.checkmark.rtl")
-    ]
-}
